77abd9e0c3222d0504525febc0a95bfba03ec7e1
Gerstl.manuel <Gerstl.manuel@EDVS205>
2025-11-28 11:28:59 +0100

    Code komentiert

--- Pi.c
+++ Pi.c
@@ -6,11 +6,11 @@
 
 int main() {
 
-    long long int num_points = 1000000;  // long long int heiﬂt sehr groﬂe Anzahl der Zufallspunkte
+    long long int num_points = 1000000;  // "long long int" heiﬂt sehr groﬂe Anzahl der Zufallspunkte
 
-    long long int inside_circle = 0; // 0 ist der Startwert an Punkten
+    long long int inside_circle = 0; // 0 ist der Startwert an Punkten 
 
-    srand(time(NULL)); //strand immer andere Zufallszahlen   time(Null) Zeit das immer andere Zufallszahlen sind
+    srand(time(NULL)); //"srand" immer andere Zufallszahlen   "time(Null)" Zeit das immer andere Zufallszahlen sind
 
     for (long long int i = 0; i < num_points; i++) {  // Wiederholt Zufallspunktgeneration 1000000 mal
 
@@ -18,9 +18,9 @@ int main() {
 
         double y = (double)rand() / RAND_MAX;  // Zuf‰lliges y zwischen 0 und 1
 
-        if (x * x + y * y <= 1.0) {   //Satz des Pythagoras <=1 ist in Kreis r=1
+        if (x * x + y * y <= 1.0) {   //Satz des Pythagoras "<=1" ist in Kreis r=1
 
-            inside_circle++;  //Wenn Punkt im Kreis +1
+            inside_circle++;      //Pr¸fen, ob der Punkt im Viertelkreis liegt wenn ja +1
 
         }
 
