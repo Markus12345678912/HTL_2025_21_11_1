93b1e6fd8b39eb3b903ca510aa0a197649b7ef07
Markus12345678912 <doktor.markus@htleisenstadt.at>
2025-12-05 11:05:05 +0100

    Update Pi.c
    Rechtschreibfehler

--- Pi.c
+++ Pi.c
@@ -6,29 +6,29 @@
 
 int main() {
 
-    long long int num_points = 1000000;  // "long long int" heiﬂt sehr groﬂe Anzahl der Zufallspunkte
+    long long int num_points = 1000000;  // "long long int" hei√üt sehr gro√üe Anzahl der Zufallspunkte
 
     long long int inside_circle = 0; // 0 ist der Startwert an Punkten 
 
-    srand(time(NULL)); //"srand" immer andere Zufallszahlen   "time(Null)" Zeit das immer andere Zufallszahlen sind
+    srand(time(NULL)); //"srand" immer andere Zufallszahlen   "time(Null)" damit immer andere Zufallszahlen sind
 
     for (long long int i = 0; i < num_points; i++) {  // Wiederholt Zufallspunktgeneration 1000000 mal
 
-        double x = (double)rand() / RAND_MAX;  // Zuf‰lliges x zwischen 0 und 1
+        double x = (double)rand() / RAND_MAX;  // Zuf√§lliges x zwischen 0 und 1
 
-        double y = (double)rand() / RAND_MAX;  // Zuf‰lliges y zwischen 0 und 1
+        double y = (double)rand() / RAND_MAX;  // Zuf√§lliges y zwischen 0 und 1
 
         if (x * x + y * y <= 1.0) {   //Satz des Pythagoras "<=1" ist in Kreis r=1
 
-            inside_circle++;      //Pr¸fen, ob der Punkt im Viertelkreis liegt wenn ja +1
+            inside_circle++;      //Pr√ºfen, ob der Punkt im Viertelkreis liegt wenn ja +1
 
         }
 
     }
 
-    double pi_estimate = 4.0 * (double)inside_circle / (double)num_points; //double das Kommazahl ok ist   4mal weil Formel f¸r Viertelkreis Bogen gilt nicht f¸r ganzen Kreis
+    double pi_estimate = 4.0 * (double)inside_circle / (double)num_points; //double das Kommazahl ok ist   4mal weil Formel f√ºr Viertelkreis Bogen gilt nicht f√ºr ganzen Kreis
 
-    printf("Gesch‰tztes Pi: %f\n", pi_estimate);  // Ausgabe Pi %f f¸r Komma
+    printf("Gesch√§tztes Pi: %f\n", pi_estimate);  // Ausgabe Pi %f f√ºr Komma
 
     return 0;
 
